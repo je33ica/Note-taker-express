fd842af05ab8861d1c5af0bf937199c225230c7c
je33ica <je331687@gmail.com>
2021-01-20 21:36:52 +0000

    started on html routes

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const router = require ('express').Router();
+const path = require ('path');
+
+router.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname,"../public/notes.html")
+    )
+});
+
+//if other route is not requested they will always be shown something - this will show index.html
+router.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"../public/index.html")
+    )
+});
+
+
+
+
+module.exports = router
