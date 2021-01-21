47b8318828f456c9fccb3c7298c1184bfee65812
je33ica <je331687@gmail.com>
2021-01-21 11:31:47 +0000

    added uuid to create unique IDs for posts

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,12 +1,17 @@
-const router = require ('express').Router();
-const path = require ('path');
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
 //using uuid to create a unique id for entries on the post route so we can delete
 //we use v4 which creats a random id
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+// Creating new unique id
+const userId = uuidv4();
+console.log(userID);
+const dB = require("../db/db.json");
 
 
 
-
+module.exports = router;
 
 // * GET `/api/notes` - Should read the `db.json` file and return all saved notes
 //  as JSON.
@@ -14,10 +19,9 @@ const { v4: uuidv4 } = require('uuid');
 // * POST `/api/notes` - Should receive a new note to save on the request body,
 //  add it to the `db.json` file, and then return the new note to the client.
 
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing 
-// the id of a note to delete. 
+// * DELETE `/api/notes/:id` - Should receive a query parameter containing
+// the id of a note to delete.
 // This means you'll need to find a way to give each note a unique `id` when it's saved.
-// In order to delete a note, you'll need to read all notes from the `db.json` file, 
-// remove the note with the given `id` property, 
+// In order to delete a note, you'll need to read all notes from the `db.json` file,
+// remove the note with the given `id` property,
 // and then rewrite the notes to the `db.json` file.
-
