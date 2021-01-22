086cfe76d37e862e9a957a7d56505a74a21d177d
je33ica <je331687@gmail.com>
2021-01-22 16:31:54 +0000

    post route with unique id's working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,5 @@
+//this api route folder is to contain the data shown to the user where they contain
+//get/post/delete
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
@@ -9,32 +11,33 @@ const userId = uuidv4();
 
 
 
-
-
 // * GET `/api/notes` - Should read the `db.json` file and return all saved notes
 //  as JSON.
 router.get("/api/notes", (req, res) =>{
 
-    res.send(json(db));
+    res.json(db);
 });
 
 // * POST `/api/notes` - Should receive a new note to save on the request body,
 //  add it to the `db.json` file, and then return the new note to the client.
 router.post("/api/notes", (req, res) => {
-    
     let newNote = {
+        //adds a unique ID using uuid so we can acces and delete later
         noteId: userId,
         title: req.body.title,
         text: req.body.text,
     };
     db.push(newNote)
      // Return the new note to the client
-     fs.writeFile("./db/db.json", JSON.stringify(db), function () {
-        res.json(db);
-      });
+     fs.writeFile("./db/db.json", JSON.stringify(db), () => {
+             res.json(db);
+         });
 })
 
+router.delete("/api/notes:id", (req, res) => {
 
+
+})
 // * DELETE `/api/notes/:id` - Should receive a query parameter containing
 // the id of a note to delete.
 // This means you'll need to find a way to give each note a unique `id` when it's saved.
