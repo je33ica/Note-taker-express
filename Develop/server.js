334ed096d84b366ca7e3280f5c974924b3b37769
je33ica <je331687@gmail.com>
2021-01-22 20:00:11 +0000

    post route saves with id and displays to client

--- Develop/server.js
+++ Develop/server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
-
 //will find the html in the public folder
 app.use(express.static("public"));
+
+
+app.use("/", apiRoutes);
 app.use("/", htmlRoute);
 
 
+//this starts our server
 app.listen(PORT, () => {console.log(`listen on ${PORT}`)});
